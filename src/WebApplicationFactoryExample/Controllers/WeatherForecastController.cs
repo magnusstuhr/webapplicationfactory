13480f33e97f1c640268b07df654050901ddbcbf
Magnus Stuhr <magnus.stuhr@partners.fortum.com>
2019-10-27 10:23:53 +0100

    Add functionality for returning a valid WeatherForecast instance on the WeatherForecastController.Get method.

--- src/WebApplicationFactoryExample/Controllers/WeatherForecastController.cs
+++ src/WebApplicationFactoryExample/Controllers/WeatherForecastController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using WebApplicationFactoryExample.Model;
 using WebApplicationFactoryExample.Services;
 
 namespace WebApplicationFactoryExample.Controllers
@@ -22,9 +19,10 @@ namespace WebApplicationFactoryExample.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
-            throw new NotImplementedException();
+            var weatherForecast = _weatherForecastService.Get();
+            return Ok(weatherForecast);
         }
     }
 }
---
+++ test/WebApplicationFactoryExampleTests/WeatherForecastControllerIntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using WebApplicationFactoryExample;
+using WebApplicationFactoryExample.Model;
+using Xunit;
+
+namespace WebApplicationFactoryExampleTests
+{
+    public class WeatherForecastControllerIntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private const string WeatherForecastUriPath = "weatherforecast";
+
+        private static HttpClient _httpClientWithFullIntegration;
+
+        private readonly WebApplicationFactory<Startup> _webApplicationFactory;
+
+        public WeatherForecastControllerIntegrationTests(WebApplicationFactory<Startup> webApplicationFactory)
+        {
+            _webApplicationFactory = webApplicationFactory;
+            _httpClientWithFullIntegration ??= webApplicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async void Get_WithFullyIntegratedServices_ReturnsOkWithExpectedResponse()
+        {
+            // Act
+            var weatherForecastResponse = await _httpClientWithFullIntegration.GetAsync(WeatherForecastUriPath);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, weatherForecastResponse.StatusCode);
+
+            var weatherForecastContent = await weatherForecastResponse.Content.ReadAsStringAsync();
+            var weatherForecast = JsonConvert.DeserializeObject<WeatherForecast>(weatherForecastContent);
+
+            Assert.NotNull(weatherForecast);
+            Assert.NotNull(weatherForecast.Summary);
+        }
+    }
+}
